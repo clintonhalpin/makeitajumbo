48e625c86682d72d1850a6f0779515f730528282
Clinton Halpin <clintonhalpin@gmail.com>
2016-02-27 12:01:10 -0500

    Initial

--- RawVMq.markdown
+++
@@ -1,7 +0,0 @@
-RawVMq
-------
-
-
-A [Pen](http://codepen.io/clintonhalpin/pen/RawVMq) by [Clinton Halpin](http://codepen.io/clintonhalpin) on [CodePen](http://codepen.io/).
-
-[License](http://codepen.io/clintonhalpin/pen/RawVMq/license).
--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 $(document).ready(function(){
     $(".container").fitVids();
     $("iframe").toggleClass('visible');
+
+    $('.js-menu').on('click', function(){
+    	$('.menu').toggleClass('visible');
+    	$('.menu ul li h1').toggleClass('animated pulse')
+    })
+	$('.menu').on('click', function(){
+    	$(this).toggleClass('visible');
+    	$('.menu ul li h1').toggleClass('animated pulse')
+    });
+
+    $(window).on('scroll', function(){
+    	console.log('holler')
+    	$('.header').addClass('dark');
+    })
 });
